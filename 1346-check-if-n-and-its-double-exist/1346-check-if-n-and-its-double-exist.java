72a4566f6d23cc4ddea786e113cb5add724161b6
DURJAY SAMRAT NANDAMUDI <111143303+durjaysamrat@users.noreply.github.com>
2025-05-17 22:55:29 +0530

    2 ms (92.62%) 43.2 MB (39.31%) 05-17-2025 .java

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            if (set.contains(2 * num) || (num % 2 == 0 && set.contains(num / 2))) {
+                return true;
+            }
+            set.add(num);
+        }
+
+        return false;
+    }
+}
