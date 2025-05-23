832d1b205c4781b1cd55c20e5832455ad18364f8
DURJAY SAMRAT NANDAMUDI <111143303+durjaysamrat@users.noreply.github.com>
2025-05-23 18:24:52 +0530

    0 ms (100%) 42.1 MB (47.71%) 05-23-2025 .java

--- 3024-type-of-triangle/3024-type-of-triangle.java
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -1,22 +1,20 @@
 class Solution {
     public String triangleType(int[] nums) {
-        int a=nums[0];
-        int b=nums[1];
-        int c=nums[2];
-        String str1="isosceles";
-        String str="equilateral";
-        String str2="scalene";
-        if(a==b&&b==c)
-        {
-          return str;
-        }
-        if(a==b||b==c||c==a)
-        {
-          return str1;
-        }
-        else
-        {
-            return str2;
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+
+        // Check for valid triangle
+        if (a + b > c && b + c > a && c + a > b) {
+            if (a == b && b == c) {
+                return "equilateral";
+            } else if (a == b || b == c || c == a) {
+                return "isosceles";
+            } else {
+                return "scalene";
+            }
+        } else {
+            return "none";
         }
     }
 }
